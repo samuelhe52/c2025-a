1bfcc6fb3bfd9049727f3c9e7f01714cca5e099c
Samuel He <samuelhe52@outlook.com>
2025-09-07 22:10:53 +0800

    Completed assignment 6

--- level1/p06_hanoi/main.c
+++ level1/p06_hanoi/main.c
@@ -1,6 +1,27 @@
 #include <printf.h>
+#include <stdlib.h>
+
+const char poles[3] = {'A', 'B', 'C'};
+
+void hanoi_move(char src, char dst, int count) {
+    if (count == 1) {
+        printf("%c -> %c\n", src, dst);
+        return;
+    }
+
+    char free_pole;
+    for (int i = 0; i < 3; i++) {
+        if (!(poles[i] == src || poles[i] == dst)) {
+            free_pole = poles[i];
+        }
+    }
+
+    hanoi_move(src, free_pole, count - 1);
+    hanoi_move(src, dst, 1);
+    hanoi_move(free_pole, dst, count - 1);
+}
 
 int main() {
-    printf("hello world!\n");
+    hanoi_move('A', 'C', 4);
     return 0;
 }
