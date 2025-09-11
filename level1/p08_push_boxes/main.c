4a174faa6267a276df5cd2befe5a9e88a0efa2d5
Samuel He <samuelhe52@outlook.com>
2025-09-11 19:48:11 +0800

    Completed basic player moving

--- level1/p08_push_boxes/main.c
+++ level1/p08_push_boxes/main.c
@@ -10,6 +10,7 @@
 #define MAZE_WIDTH 8
 #define MAZE_HEIGHT 8
 
+// x is vertical, y is horizontal
 typedef struct pos {
     int x;
     int y;
@@ -23,6 +24,15 @@ pos dest_poses[5];
 bool is_box(const int x, const int y) {
     for (int i = 0; i < 5; i++) {
         if (box_poses[i].x == x && box_poses[i].y == y) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool is_dest(const int x, const int y) {
+    for (int i = 0; i < 5; i++) {
+        if (dest_poses[i].x == x && dest_poses[i].y == y) {
             return true;
         }
     }
@@ -48,20 +58,18 @@ void initialize_map(void) {
         fgets(line, 50, fp);
         for (int j = 0; j < MAZE_WIDTH; j++) {
             if (line[j] == PLAYER) {
-                player_pos.y = i;
-                player_pos.x = j;
+                player_pos.x = i;
+                player_pos.y = j;
             } else if (line[j] == BOX) {
-                box_poses[box_counter].y = i;
-                box_poses[box_counter].x = j;
+                box_poses[box_counter].x = i;
+                box_poses[box_counter].y = j;
                 box_counter++;
             } else if (line[j] == DEST) {
-                maze[i][j] = DEST;
-                dest_poses[dest_counter].y = i;
-                dest_poses[dest_counter].x = j;
+                dest_poses[dest_counter].x = i;
+                dest_poses[dest_counter].y = j;
                 dest_counter++;
-            } else {
-                maze[i][j] = (line[j] == '1') ? WALL : SPACE;
             }
+            maze[i][j] = (line[j] == '1') ? WALL : SPACE;
         }
     }
 
@@ -71,11 +79,11 @@ void initialize_map(void) {
 void print_map() {
     for (int row = 0; row < MAZE_HEIGHT; row++) {
         for (int col = 0; col < MAZE_WIDTH; col++) {
-            if (is_player(col, row)){
+            if (is_player(row, col)){
                 printw("%c", PLAYER);
-            } else if (is_box(col, row)) {
+            } else if (is_box(row, col)) {
                 printw("%c", BOX);
-            } else if (maze[row][col] == DEST) {
+            } else if (is_dest(row, col)) {
                 printw("%c", DEST);
             } else {
                 printw("%c", maze[row][col]);
@@ -87,6 +95,40 @@ void print_map() {
     refresh();
 }
 
+void refresh_map(void) {
+    clear();
+    refresh();
+    print_map();
+}
+
+void move_left() {
+    if (maze[player_pos.x][player_pos.y - 1] == SPACE) {
+        player_pos.y--;
+        refresh_map();
+    }
+}
+
+void move_right() {
+    if (maze[player_pos.x][player_pos.y + 1] == SPACE) {
+        player_pos.y++;
+        refresh_map();
+    }
+}
+
+void move_up() {
+    if (maze[player_pos.x - 1][player_pos.y] == SPACE) {
+        player_pos.x--;
+        refresh_map();
+    }
+}
+
+void move_down() {
+    if (maze[player_pos.x + 1][player_pos.y] == SPACE) {
+        player_pos.x++;
+        refresh_map();
+    }
+}
+
 int main() {
     initscr(); // Initialize ncurses
     raw();  // Disable line buffering
@@ -95,5 +137,34 @@ int main() {
 
     initialize_map();
     print_map();
+
+    int ch;
+    while ((ch = getch()) != 'q') {
+        switch (ch) {
+            case KEY_UP:
+                move_up();
+                break;
+            case KEY_DOWN:
+                move_down();
+                break;
+            case KEY_LEFT:
+                move_left();
+                break;
+            case KEY_RIGHT:
+                move_right();
+                break;
+            default:
+                printw("\a");
+                refresh();
+                break;
+        }
+        // if (player_pos.x == exit_pos.x && player_pos.y == exit_pos.y) {
+        //     printw("Congratulations! You've reached the exit!\n");
+        //     refresh();
+        //     break;
+        // }
+        refresh();
+    }
+
     return 0;
 }
